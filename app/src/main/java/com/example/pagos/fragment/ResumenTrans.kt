2e54187517b8ccfa5029e394a98665aa84ff40d0
Magner Rivera <magner.rivera4@gmail.com>
2022-05-22 16:44:27 -0500

    -Ajuste: -Se agrega validacion para los campos vacios y se agrea validación para el rango de las tarjetas -Se ajusta la navegación

--- app/src/main/java/com/example/pagos/fragment/ResumenTrans.kt
+++ app/src/main/java/com/example/pagos/fragment/ResumenTrans.kt
@@ -1,14 +1,15 @@
 package com.example.pagos.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.example.pagos.R
-import org.w3c.dom.Text
+import com.example.rickandmorty.utils.navigateTo
 
 class ResumenTrans : Fragment() {
     val args: ResumenTransArgs by navArgs()
@@ -18,6 +19,7 @@ class ResumenTrans : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_resumen_trans, container, false)
         val datos = args.datos
+        val back = view.findViewById(R.id.btn_cancelar) as Button
         view.findViewById<TextView>(R.id.traerEstado).apply {
             text = datos.estado
         }
@@ -33,6 +35,10 @@ class ResumenTrans : Fragment() {
         view.findViewById<TextView>(R.id.traerFecha).apply {
             text = datos.fecha
         }
+        back.setOnClickListener {
+            navigateTo(ResumenTransDirections.actionResumenTransToOpcion(args.datos))
+        }
+
         return view
     }
 
